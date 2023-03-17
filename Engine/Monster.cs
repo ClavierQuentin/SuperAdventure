ee8a60ee732b63c9d0478fd7ba351715bbe031d4
Quentin Clavier <clavier.quentin@gmail.com>
2023-03-17 15:09:42 +0100

    add world constructor & UI

---
+++ Engine/Monster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = null!;
+        public int MaximunDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<LootItem>LootTable { get; set; }
+        public Monster( int id, string name, int maximunDamage, int rewardExperiencePoints, int rewardGold, int maxHitPoints, int currentHitPoints) : base(maxHitPoints, currentHitPoints)
+        {
+            ID = id;
+            Name = name;
+            MaximunDamage = maximunDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
+        }
+    }
+}
---
+++ Engine/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Weapon : Item
+    {
+        public int MinimunDamage { get; set; }
+        public int MaximunDamage { get; set; }
+        public Weapon(int id, string name, string namePlural, int minimunDamage, int maximunDamage):base(id, name, namePlural)
+        {
+            MinimunDamage = minimunDamage;
+            MaximunDamage = maximunDamage;
+        }
+    }
+}
